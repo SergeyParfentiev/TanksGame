5b0a6e8bc096aae4cb07dbc1d96bc2c03b0fc20d
Sergey <bouth@mail.ru>
2015-12-12 12:08:36 +0200

    Rejected commit: Default

---
+++ IdeaProgrammist/Collections/src/List/OwnImplementation/SimpleLinkedList.java
@@ -0,0 +1,32 @@
+package List.OwnImplementation;
+
+/**
+ * Created by Byblik272 on 12/12/2015.
+ */
+public class SimpleLinkedList {
+
+    private Node root;
+    private int size;
+
+    public void addFirst(Object o) {
+    }
+    public void addLast(Object o) {
+    }
+    public void addAfter(Object o, Object obj) {
+    }
+    public int getSize() {
+        return size;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
