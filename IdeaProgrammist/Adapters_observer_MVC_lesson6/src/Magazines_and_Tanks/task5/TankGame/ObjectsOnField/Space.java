824e2554f9124c76735acd4b9394d2e3435edd15
Sergey <bouth@mail.ru>
2016-01-12 19:58:18 +0200

    Rejected commit: Default

---
+++ IdeaProgrammist/Adapters_observer_MVC_lesson6/src/Magazines_and_Tanks/task5/TankGame/ObjectsOnField/Space.java
@@ -0,0 +1,24 @@
+package Magazines_and_Tanks.task5.TankGame.ObjectsOnField;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Byblik272 on 2/12/2015.
+ */
+public class Space extends AbstractObjectOnField {
+
+    public Space(int x, int y) {
+        super(x, y);
+        color = new Color(180, 180, 180);
+        try {
+            image = ImageIO.read(new File("Spaces.png").getAbsoluteFile());
+        } catch (IOException e) {
+
+        }
+    }
+
+
+}
---
+++ IdeaProgrammist/Adapters_observer_MVC_lesson6/src/Magazines_and_Tanks/task5/TankGame/Tanks/Action.java
@@ -0,0 +1,8 @@
+package Magazines_and_Tanks.task5.TankGame.Tanks;
+
+public enum Action {
+	
+	NONE, MOVE, FIRE;
+	
+//	, TURN_UP, TURN_RIGHT, TURN_LEFT, TURN_DOWN;
+}
