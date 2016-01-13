590de70baed10900cac0a04269a30415c1fdf343
Sergey <bouth@mail.ru>
2016-01-13 14:08:38 +0200

    Rejected commit: Default

---
+++ IdeaProgrammist/Adapters_observer_MVC_lesson6/src/HomeTask/task7/TankGame/Run/Launcher.java
@@ -0,0 +1,14 @@
+package HomeTask.task7.TankGame.Run;
+
+import HomeTask.task7.TankGame.Action.ActionField;
+
+/**
+ * Created by Byblik272 on 18/11/2015.
+ */
+public class Launcher {
+    public static void main(String[] args) throws Exception {
+
+        ActionField bf = new ActionField();
+        bf.runTheGame();
+    }
+}
