9614141c051d3bb18ee4db47eaf3a2c14a248cda
Sergey <bouth@mail.ru>
2016-01-14 22:54:00 +0200

    Rejected commit: Default

---
+++ IdeaProgrammist/Concept_Of_The_Box_Notes_Unit_Tests_lesson7/src/Container_And_Annotations/task2/Cleaning.java
@@ -0,0 +1,8 @@
+package Container_And_Annotations.task2;
+
+/**
+ * Created by Byblik272 on 14/1/2016.
+ */
+public class Cleaning implements Service{
+
+}
---
+++ IdeaProgrammist/Concept_Of_The_Box_Notes_Unit_Tests_lesson7/src/Container_And_Annotations/task2/ServiceRepository_Launcher.java
@@ -0,0 +1,12 @@
+package Container_And_Annotations.task2;
+
+/**
+ * Created by Byblik272 on 14/1/2016.
+ */
+public class ServiceRepository_Launcher {
+    public static void main(String[] args) {
+
+        ServiceRepository<Service> service = new ServiceRepository<>();
+        ServiceRepository<Cleaning> clining = new ServiceRepository<>();
+    }
+}
