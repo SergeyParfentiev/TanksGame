649eb45495aeeb1de8b1c8e53b62191ae55d383f
Sergey <bouth@mail.ru>
2016-01-21 19:15:03 +0200

    Rejected commit: Default

---
+++ IdeaProgrammist/Concept_Of_The_Box_Notes_Unit_Tests_lesson7/src/Reflection/task6/Service.java
@@ -0,0 +1,15 @@
+package Reflection.task6;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Byblik272 on 21/1/2016.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Service {
+    String test();
+}
