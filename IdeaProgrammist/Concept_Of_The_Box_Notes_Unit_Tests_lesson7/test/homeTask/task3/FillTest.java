f0e535b879a17a5f96b5a115928e203b7d72611c
Sergey <bouth@mail.ru>
2016-01-25 15:37:31 +0200

    Rejected commit: Default

---
+++ IdeaProgrammist/Concept_Of_The_Box_Notes_Unit_Tests_lesson7/test/homeTask/task3/FillTest.java
@@ -0,0 +1,44 @@
+package homeTask.task3;
+
+import Reflection.task4.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Byblik272 on 25/1/2016.
+ */
+@RunWith(JUnit4.class)
+public class FillTest {
+
+    Fill<Bird> fill;
+    List<Object> list;
+    Bird bird;
+
+    @Before
+    public void init(){
+        fill = new Fill();
+    }
+
+    @Test
+    public void initClassDefaultTest() throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        list = new ArrayList<>();
+        bird = fill.initClass(Bird.class, list);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void initClassOptionsTest() throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        list = new ArrayList<>();
+        list.add("Options");
+        list.add("not");
+        list.add("correct");
+        bird = fill.initClass(Bird.class, list);
+    }
+}
