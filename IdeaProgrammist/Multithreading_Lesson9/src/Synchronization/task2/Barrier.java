fd688bedc6539b3e45c3eefcb2eec386ec88b7fa
Sergey <bouth@mail.ru>
2016-02-23 19:00:28 +0200

    Rejected commit: Default

---
+++ IdeaProgrammist/Multithreading_Lesson9/src/Synchronization/task2/Barrier.java
@@ -0,0 +1,26 @@
+package Synchronization.task2;
+
+import java.awt.*;
+
+/**
+ * Created by Byblik272 on 23/2/2016.
+ */
+public class Barrier {
+
+    public int x = 100;
+    public int y = 150;
+
+    public int barrierEndX = 300;
+    public int barrierEndY = 150;
+
+    public int bottomY = 200;
+
+    public int open = 200;
+    public int close = 300;
+
+    public int thickness = 20;
+
+    public Color color = Color.RED;
+
+    public boolean isOpen = false;
+}
---
+++ IdeaProgrammist/Multithreading_Lesson9/src/Synchronization/task2/Car.java
@@ -0,0 +1,15 @@
+package Synchronization.task2;
+
+/**
+ * Created by Byblik272 on 23/2/2016.
+ */
+public class Car {
+
+    public int x = 110;
+    public int y = 200;
+
+    public int width = 150;
+    public int height = 100;
+
+    public int endPoint = 100;
+}
